da4f8b7b9a2408a8c0ebf97c0e323e2449a2a9a9
NightMoonCat <495982617@qq.com>
2017-04-12 20:00:59 +0800

    实现在本地服务器和环信服务器验证和登录帐号

--- app/src/main/java/cn/moon/live/LiveApplication.java
+++ app/src/main/java/cn/moon/live/LiveApplication.java
@@ -14,52 +14,54 @@ import cn.moon.live.ui.activity.MainActivity;
 /**
  * Created by wei on 2016/5/27.
  */
-public class LiveApplication extends Application{
+public class LiveApplication extends Application {
 
-  private static LiveApplication instance;
-
-
-  @Override public void onCreate() {
-    super.onCreate();
-    instance = this;
+    private static LiveApplication instance;
 
 
-    initChatSdk();
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
 
-    //UEasyStreaming.initStreaming("publish3-key");
 
-    UStreamingContext.init(getApplicationContext(), "publish3-key");
-  }
+        initChatSdk();
 
-  public static LiveApplication getInstance(){
-    return instance;
-  }
+        //UEasyStreaming.initStreaming("publish3-key");
 
-  private void initChatSdk(){
-    //EMOptions options = new EMOptions();
-    //options.enableDNSConfig(false);
-    //options.setRestServer("120.26.4.73:81");
-    //options.setIMServer("120.26.4.73");
-    //options.setImPort(6717);
+        UStreamingContext.init(getApplicationContext(), "publish3-key");
+    }
 
-    EaseUI.getInstance().init(this, null);
-    EMClient.getInstance().setDebugMode(true);
+    public static LiveApplication getInstance() {
+        return instance;
+    }
 
-    EMClient.getInstance().addConnectionListener(new EMConnectionListener() {
-      @Override public void onConnected() {
+    private void initChatSdk() {
+        //EMOptions options = new EMOptions();
+        //options.enableDNSConfig(false);
+        //options.setRestServer("120.26.4.73:81");
+        //options.setIMServer("120.26.4.73");
+        //options.setImPort(6717);
 
-      }
+        EaseUI.getInstance().init(this, null);
+        EMClient.getInstance().setDebugMode(true);
 
-      @Override public void onDisconnected(int errorCode) {
-        if(errorCode == EMError.USER_LOGIN_ANOTHER_DEVICE)
-        {
-          Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-          intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-          intent.putExtra("conflict", true);
-          startActivity(intent);
-        }
-      }
-    });
-  }
+        EMClient.getInstance().addConnectionListener(new EMConnectionListener() {
+            @Override
+            public void onConnected() {
+
+            }
+
+            @Override
+            public void onDisconnected(int errorCode) {
+                if (errorCode == EMError.USER_LOGIN_ANOTHER_DEVICE) {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.putExtra("conflict", true);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
 
 }
