d8a027a5434c43cdaaa024ffa4a6e7abdff3a014
NightMoonCat <495982617@qq.com>
2017-04-12 17:09:16 +0800

    添加接口文件和工具类，在接口文件里添加直播项目需要的常量和字符串

---
+++ app/src/main/java/cn/moon/live/utils/MFGT.java
@@ -0,0 +1,34 @@
+package cn.moon.live.utils;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import cn.moon.live.R;
+
+
+/**
+ * Created by Moon on 2017/3/16.
+ */
+
+public class MFGT {
+    public static void startActivity(Activity activity, Class cls) {
+        activity.startActivity(new Intent(activity, cls));
+        activity.overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
+    }
+
+    public static void startActivity(Activity activity, Intent intent) {
+        activity.startActivity(intent);
+        activity.overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
+    }
+
+    public static void finish(Activity activity) {
+        activity.finish();
+        activity.overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+    }
+
+
+    public static void startActivityForResult(Activity activity, Intent intent, int requestCode) {
+        activity.startActivityForResult(intent, requestCode);
+        activity.overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
+    }
+}
