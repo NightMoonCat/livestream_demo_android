a772e545c9b4504f95694d05575d69cc2928f42f
NightMoonCat <495982617@qq.com>
2017-04-14 21:13:00 +0800

    新建GiftDao，添加字段和表名

---
+++ app/src/main/java/cn/moon/live/db/GiftDao.java
@@ -0,0 +1,20 @@
+package cn.moon.live.db;
+
+import android.content.Context;
+
+/**
+ * Created by Moon on 2017/4/14.
+ */
+
+public class GiftDao {
+    public static final String GIFT_TABLE_NAME = "gifts";
+    public static final String GIFT_COLUMN_NAME = "gName";
+    public static final String GIFT_COLUMN_ID = "gId";
+    public static final String GIFT_COLUMN_URL = "gUrl";
+    public static final String GIFT_COLUMN_PRICE = "gPrice";
+
+    public GiftDao(Context context) {
+
+    }
+
+}
